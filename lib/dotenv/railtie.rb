978a5af48f696cbf061a429f686dc9a4be5963f9
Donald Piret <donald@donaldpiret.com>
2014-03-16 02:36:27 +0800

    Use overload instead of load

--- lib/dotenv/railtie.rb
+++ lib/dotenv/railtie.rb
@@ -5,10 +5,10 @@ module Dotenv
     rake_tasks do
       desc 'Load environment settings from .env'
       task :dotenv do
-        Dotenv.load ".env.#{Rails.env}", '.env'
+        Dotenv.overload ".env.#{Rails.env}", '.env'
       end
     end
   end
 end
 
-Dotenv.load ".env.#{Rails.env}", '.env'
+Dotenv.overload ".env.#{Rails.env}", '.env'
